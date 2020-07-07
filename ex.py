0b4c6e1bec463fd0f96b4b4ff04b85afcf0deb98
oldlwhite <2216403312@qq.com>
2020-07-07 23:28:01 +0800

    '666'

--- ex.py
+++ ex.py
@@ -4,6 +4,7 @@ import bs4
 import multiprocessing
 requests.packages.urllib3.disable_warnings()
 headers={
+
     "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36"
 }
 def get_m3u8(url):
